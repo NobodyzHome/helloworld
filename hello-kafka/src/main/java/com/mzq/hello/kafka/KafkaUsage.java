a465c7512d8b77489468e8cb06929c68867099eb
maziqiang <maziqiang@jd.com>
2023-02-07 22:11:18 +0800

    增加kafka幂等性、事务的使用

--- hello-kafka/src/main/java/com/mzq/hello/kafka/KafkaUsage.java
+++ hello-kafka/src/main/java/com/mzq/hello/kafka/KafkaUsage.java
@@ -3,24 +3,37 @@ package com.mzq.hello.kafka;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class KafkaUsage {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "my-producer");
+        properties.put(ProducerConfig.ACKS_CONFIG, "-1");
+        properties.put(ProducerConfig.RETRIES_CONFIG, "5");
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
-        for (int i = 100001; i <= 10000000; i++) {
+        for (int i = 100001; i <= 100101; i++) {
             ProducerRecord<String, String> producerRecord = new ProducerRecord<>("hello_world", "hello" + i, "hello world" + i);
-            kafkaProducer.send(producerRecord);
+            Future<RecordMetadata> send = kafkaProducer.send(producerRecord);
+            if(!send.isDone()){
+                System.out.println("test");
+            }
+            RecordMetadata recordMetadata = send.get();
         }
         kafkaProducer.close();
     }
+
+
 }
--- hello-kafka/src/test/java/com/mzq/hello/flink/ProducerTest.java
+++ hello-kafka/src/test/java/com/mzq/hello/flink/ProducerTest.java
@@ -11,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.SerializationUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
@@ -63,6 +60,10 @@ public class ProducerTest {
      * 1.从RecordAccumulator中拉取ProducerBatch，然后转换成Request，发送到partition对应的broker节点
      * 2.监听kafka broker对Request给出的响应，根据响应结果清理RecordAccumulator中已响应的ProducerBatch
      * 3.根据broker响应中返回的异常，将可重试异常的ProducerBatch重新加入到RecordAccumulator中
+     * 注意：
+     * 1.用户线程起始于调用KafkaProducer的send方法，结束于send方法内部将ProducerBatch存入RecordAccumulator，返回的Future是将来sender线程将ProducerBatch发送到broker后，broker对发送结果的响应。(类似hbase的put方法，只需要把数据写入到memstore，即完成了写入流程)
+     * 2.sender线程起始于从RecordAccumulator中拉取ProducerBatch，结束于接收到broker的响应并清理已处理完的RecordAccumulator的ProducerBatch，并把用户线程send方法返回的Future置为处理完成
+     * 3.无论ProducerRecord是在RecordAccumulator还是在in-flight request中，数据还是在producer客户端中，没有被broker记录。只有request被发送到broker并且broker成功响应后，数据才真正存储到了broker中，才可以被消费者消费。
      */
     @Test
     public void basicUsage() throws ExecutionException, InterruptedException, TimeoutException {
@@ -92,7 +93,7 @@ public class ProducerTest {
          * 3.ack=all或-1，broker在收到该请求后，会将数据写入到leader副本，然后等待ISR中的其他broker同步完该条数据，才会给出响应。因此send方法返回的RecordMetadata中也会带有数据写入到副本的实际的offset。
          *
          * 注意：
-         * 1.无论ack等于几，都是Sender线程接收来自kafka的响应，然后根据响应内容把Future<RecordMetadata>设置为done状态，这样用户线程就可以获取到kafka的响应结果了（在此之前调用get方法会被阻塞）
+         * 1.无论ack等于几，producer都是异步发送record，即producer只把record加入到内存中的RecordAccumulator就结束了，用户线程就不再阻塞了。而ack影响的是send方法返回的Future对象被响应的速度，该Future对象只有在broker对其发送响应后才会被赋值。
          * 2.如果ack=0，那么retries就无效了，因为客户端并没有等到broker对请求的响应结果，因此也就不知道响应结果中是否有异常，导致无法判断是否需要重试
          */
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "1");
@@ -117,14 +118,14 @@ public class ProducerTest {
          */
         properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "1000");
         /*
-        * 当我们发送消息时，为了减少网络I/O的使用，producer在发送的ProducerRecord时，会把数据先存储到内存中，具体是存储到ProducerBatch中。此后，producer会在以下情况将内存中的ProducerBatch发送给broker：
-        * 1.ProducerBatch的大小超过batch.size，代表该ProducerBatch已经装满数据了，可以被发送出去了
-        * 2.ProducerBatch的大小虽然小于batch.size，但在RecordAccumulator中存在的时间也超过了linger.ms时间。
-        * linger.ms配置的目的是：让ProducerBatch即使没有满足大小的要求，也可以及时发送到broker中，不会因为一直不生产新的ProducerRecord，导致数据都积压在producer的内存中
-        *
-        * 注意：一个ProducerBatch如果在RecordAccumulator中存在时间超过delivery.timeout.ms后也没发送到broker中，那么KafkaProducer会抛出异常。因此linger.ms应该远小于delivery.timeout.ms，
-        * 让producer尽量不会因为ProducerBatch存储在内存中时间过长而抛出异常。
-        */
+         * 当我们发送消息时，为了减少网络I/O的使用，producer在发送的ProducerRecord时，会把数据先存储到内存中，具体是存储到ProducerBatch中。此后，producer会在以下情况将内存中的ProducerBatch发送给broker：
+         * 1.ProducerBatch的大小超过batch.size，代表该ProducerBatch已经装满数据了，可以被发送出去了
+         * 2.ProducerBatch的大小虽然小于batch.size，但在RecordAccumulator中存在的时间也超过了linger.ms时间。
+         * linger.ms配置的目的是：让ProducerBatch即使没有满足大小的要求，也可以及时发送到broker中，不会因为一直不生产新的ProducerRecord，导致数据都积压在producer的内存中
+         *
+         * 注意：一个ProducerBatch如果在RecordAccumulator中存在时间超过delivery.timeout.ms后也没发送到broker中，那么KafkaProducer会抛出异常。因此linger.ms应该远小于delivery.timeout.ms，
+         * 让producer尽量不会因为ProducerBatch存储在内存中时间过长而抛出异常。
+         */
         properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "600");
 
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
@@ -492,9 +493,9 @@ public class ProducerTest {
         /*
          * partitioner.class用来决定一条ProducerRecord数据应发送到哪个分区，如果不设置，默认使用的是org.apache.kafka.clients.producer.internals.DefaultPartitioner
          * 它的处理策略如下：
-         * If a partition is specified in the record, use it
+         * 如果用户指定了要发往的分区，则使用该分区。If a partition is specified in the record, use it
          * If no partition is specified but a key is present choose a partition based on a hash of the key
-         * If no partition or key is present choose the sticky partition（这里的sticky partition，大概就是说从可用的分区cluster.availablePartitionsForTopic(topic)中随机选择一个） that changes when the batch is full
+         * If no partition or key is present choose the sticky partition that changes when the batch is full
          * 关于它的处理策略的补充说明：
          * 1.对于有key的ProducerRecord，它可以保证拥有相同key的数据都存储在一个分区里，我们知道，kafka可以保证在一个分区里的数据的顺序
          * 2.对于没有key也没有在ProducerRecord中指定分区的，它的策略是使用sticky partition，大概意思就是如果可用分区（cluster.availablePartitionsForTopic(topic)）>0，则从可用分区中随机选择一个，否则从全部分区（cluster.partitionsForTopic(topic)）中随机选择一个
@@ -522,6 +523,191 @@ public class ProducerTest {
                     log.info("without key.waybillCode={},partition={},offset={}", bdWaybillOrder.getWaybillCode(), metadata.partition(), metadata.offset());
                 });
             }
+        }
+    }
+
+    /**
+     * 幂等性主要解决的问题是数据重复存储。假设有一个record从producer发送到broker，broker收到后进行落盘，在落完盘后，给producer发送响应前发生了异常。
+     * 此时broker会给producer响应异常信息，producer此时如果开启的retry后，会再次将该record发送到broker，此时broker落完盘后，可以发现该record在broker中重复落盘了两次。
+     * 幂等性就是为了解决该问题，针对已落盘而又重复发过来的数据，broker会抛弃，而不是继续落盘，避免了一个record的重复存储。
+     * 当我们启动了幂等性，就能够保证producer往broker发送【一个】record时，可以以【exactly once】的语义进行数据传输，即既不会丢，也不会重复，一个record在broker中仅存储一次。
+     */
+    @Test
+    public void testIdempotence() {
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
+        producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "idempotence-cli");
+        producerProperties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, String.valueOf(DataSize.ofKilobytes(32).toBytes()));
+        producerProperties.setProperty(ProducerConfig.BUFFER_MEMORY_CONFIG, String.valueOf(DataSize.ofMegabytes(64).toBytes()));
+        // 启动幂等需要增加以下配置：
+        // 1.enable.idempotence=true
+        // 2.acks=all
+        // 3.retries>0
+        // 4.max.in.flight.requests.per.connection<=5
+        producerProperties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        producerProperties.setProperty(ProducerConfig.ACKS_CONFIG, "-1");
+        producerProperties.setProperty(ProducerConfig.RETRIES_CONFIG, "10");
+        producerProperties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        producerProperties.setProperty(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "1000");
+        producerProperties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, String.valueOf(Duration.ofSeconds(30).toMillis()));
+        producerProperties.setProperty(ProducerConfig.LINGER_MS_CONFIG, String.valueOf(Duration.ofSeconds(1).toMillis()));
+        producerProperties.setProperty(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, String.valueOf(Duration.ofSeconds(29).toMillis()));
+
+        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(producerProperties)) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("hello_idempotence", "hello", "world");
+            Future<RecordMetadata> recordMetadataFuture = kafkaProducer.send(producerRecord);
+            // 由于acks=all，因此只有在ISR的所有副本都写入完毕后，producer才会获得响应，get方法才会解除阻塞
+            RecordMetadata recordMetadata = recordMetadataFuture.get();
+            System.out.printf("partition=%d,offset=%d,timestamp=%d%n",recordMetadata.partition(),recordMetadata.offset(),recordMetadata.timestamp());
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 事务配置总结
+     * 1.在consume - transform - produce的consumer端
+     * a) 关闭自动提交位点
+     *      enable.auto.commit=false
+     * b) 程序中也不要手动提交位点
+     * <p>
+     * 2.在consume - transform - produce的producer端
+     * a) 启动幂等性功能：
+     *      enable.idempotence=true
+     *      acks=all
+     *      retries>0
+     *      max.in.flight.requests.per.connection<=5
+     * b) 设置一个独一无二的事务id
+     *      transactional.id=xxx
+     * <p>
+     * 3.在消费producer发送的topic的consumer端
+     * a) 事务隔离性设置为read_committed
+     *      isolation.level=read_committed
+     * <p>
+     * kafka事务与mysql事务的区别是：
+     * kafka的事务是在producer客户端端生成的，不同的producer在往同一个topic中发送数据时，可以一个producer以事务的方式发送，另一个以非事务的方式发送。同时consumer在消费topic时，也可以选择读不读取事务中未提交的数据。
+     * mysql事务是在mysql实例端的，客户端在插入数据时，是否启动事务也是由客户端决定。但在读取数据时，是由mysql实例决定事务的隔离级别，而不是客户端自行决定。
+     */
+    @Test
+    public void testTransaction() {
+         /*
+             事务的典型应用模型是consume-transform-produce，也就是将数据拉取、数据加工、数据发送视作一个原子性操作，包含在一个事务里。只有这批数据全部处理完毕，才能事务提交。这批数据中任意一条数据发生处理问题，都会进行事务回滚。
+             举例来说，程序中groupId=myGroup的consumer从topicA拉取到了R1、R2、R3这三条数据，然后对其进行数据加工，最终将这三条数据发送到topicB。
+             只有在R1、R2、R3都发送到topicB了，才能提交事务，此时会：
+             a) 将topicA中myGroup这个group的位点提交到R3的offset + 1
+             b) 将topicB中R1、R2、R3置为已提交状态，这样topicB的consumer(read_committed)就可以拉取到这三条数据了
+             如果R1已经处理完毕并发送到topicB，但在处理R2时发生了异常，此时会回滚事务，此时会：
+             a) 不提交topicA中myGroup的位点，即使R1已经被读取并处理完了
+             b) 将已经向topicB发送的R1这条数据置成回滚状态，这样topicB的consumer(read_committed)就不能拉取能R1这条数据了，即使R1实际已经发送到topicB中了
+
+             幂等的启动保证了producer往一个topic的一个partition发送一个record时，既不会丢也不会重复。而事务则是保证一批数据在发往不同topic的不同partition，要么全部成功，要么全部失败，避免部分成功，部分失败，最终造成破坏【数据一致性】的问题
+             什么时候需要事务：
+             1.要往topic里发送一批数据。一次只发送一个record的话，只需要使用幂等性就可以了。
+             2.这批数据具有【数据一致性】的特征。消费者不能只读取其中的一部分数据，需要消费者要么全部读取到，要么全都不读取。
+             什么是数据一致性？
+             比如有一笔A到B的转账，产生了两条数据，一个是A减少100，另一个是B增加100。这两条数据要么全部处理完，要么全部不处理，因此这两条数据具有数据一致性特征。假如消费者只读取到了A减少100，B增加100由于生产者处理出错而没有发出，那么A就白白减少了100，数据的一致性就被破坏了。
+         */
+        Properties consumerProperties = new Properties();
+        consumerProperties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
+        consumerProperties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "ctp-group");
+        consumerProperties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "ctp-cli");
+        // 在consume-transform-produce模式中：
+        // 1.必须要关闭自动提交位点
+        // 2.也不能在程序中手动提交位点
+        // 因为需要在producer提交事务成功后，才提交位点
+        consumerProperties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumerProperties.setProperty(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, "1048576");
+        consumerProperties.setProperty(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "1000");
+        consumerProperties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProperties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
+        producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "tx-producer");
+        // 开启事务的前提时开启幂等性
+        producerProperties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        producerProperties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        producerProperties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
+        producerProperties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        producerProperties.setProperty(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "500");
+        // 启动事务后还需要设置一个事务id，每个生产者的事务id应该都是独一无二的
+        producerProperties.setProperty(ProducerConfig.TRANSACTIONAL_ID_CONFIG, UUID.randomUUID().toString());
+        producerProperties.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, "10000");
+
+        KafkaProducer<String, String> kafkaProducer = null;
+        KafkaConsumer<String, String> kafkaConsumer = null;
+        // 下面这些代码主要有以下操作：
+        // 1.consume:从hello_world中拉取一批数据
+        // 2.transform:对这批数据进行逻辑处理
+        // 3.produce:将处理结果发送只hello_tx
+        // 使用事务后，上面这三个操作则被看作一个原子操作，只有所有数据全部处理成功才算成功，处理任意一个record发送错误，所有数据都会失败(包含已处理的数据)。
+        try {
+            kafkaProducer = new KafkaProducer<>(producerProperties);
+            kafkaConsumer = new KafkaConsumer<>(consumerProperties);
+            kafkaProducer.initTransactions();
+            kafkaProducer.beginTransaction();
+            kafkaConsumer.subscribe(Collections.singleton("hello_world"));
+            // 从hello_world拉取一批数据
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(2));
+            Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>(consumerRecords.partitions().size());
+            for (ConsumerRecord<String, String> record : consumerRecords) {
+                // 对拉取的数据进行处理
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("hello_tx", record.key(), String.format("%s#%s", record.value(), "_tx"));
+                // 将处理后的数据发送至hello_tx
+                kafkaProducer.send(producerRecord);
+            }
+            for (TopicPartition partition : consumerRecords.partitions()) {
+                List<ConsumerRecord<String, String>> partitionRecords = consumerRecords.records(partition);
+                ConsumerRecord<String, String> lastRecord = partitionRecords.get(partitionRecords.size() - 1);
+                offsets.put(partition, new OffsetAndMetadata(lastRecord.offset() + 1));
+            }
+            // 使用sendOffsetsToTransaction方法设置在事务提交后，consume - transform - produce中consumer需要提交的位点信息
+            kafkaProducer.sendOffsetsToTransaction(offsets, kafkaConsumer.groupMetadata());
+            // 提交事务，只有提交事务后，事务中发送的record才可以被consumer拉取到
+            kafkaProducer.commitTransaction();
+        } catch (Exception e) {
+            // 当发生异常后，回滚事务，此时事务中已发送的record都不会被consumer拉取到
+            if (Objects.nonNull(kafkaProducer)) {
+                kafkaProducer.abortTransaction();
+            }
+        } finally {
+            if (Objects.nonNull(kafkaProducer)) {
+                kafkaProducer.close();
+            }
+            if (Objects.nonNull(kafkaConsumer)) {
+                kafkaConsumer.close();
+            }
+        }
+    }
+
+    @Test
+    public void testTransactionConsumer() {
+        Properties consumerConfig = new Properties();
+        consumerConfig.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
+        consumerConfig.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerConfig.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerConfig.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "tx-group");
+        consumerConfig.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "tx-client");
+        consumerConfig.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        // 事务的处理不只包含生产者，也包含消费者。消费者需要设置事务隔离级别为read_committed，让消费者拉取不到未提交事务中发送的record。
+        // consumer默认的事务隔离级别是read_uncommitted，所以生产者以事务的形式往一个topic里发送record时，所有consumer在消费这个topic也需要设置事务隔离级别为read_committed。
+        consumerConfig.setProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+        consumerConfig.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerConfig)) {
+            kafkaConsumer.subscribe(Collections.singleton("hello_tx"));
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(2));
+                consumerRecords.forEach(System.out::println);
+                kafkaConsumer.commitSync();
+            }
+
         }
     }
 }
