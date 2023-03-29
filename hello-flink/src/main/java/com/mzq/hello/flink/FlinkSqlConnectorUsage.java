252125e0d43554caf00e10e5c065b6fa796489bb
maziqiang <maziqiang@jd.com>
2023-03-29 14:40:47 +0800

    升级flink1.17

--- hello-flink/src/main/java/com/mzq/hello/flink/FlinkSqlConnectorUsage.java
+++ hello-flink/src/main/java/com/mzq/hello/flink/FlinkSqlConnectorUsage.java
@@ -15,7 +15,7 @@ public class FlinkSqlConnectorUsage {
 //        tableEnvironment.executeSql("insert into sink_table " +
 //                "select t1.id,t1.name,lt.rst from (select * from (values(1,'aa',proctime()),(2,'ba',proctime()),(3,'ca',proctime())) as t(id,name,row_time)) t1 left join lookup_table for system_time as of t1.row_time as lt on t1.name=lt.name and t1.id=lt.id");
 
-        tableEnvironment.executeSql("create table redis_sink(ttl bigint metadata from 'the_ttl',jim_key string,jim_value string) with('connector'='redis','redis.url'='redis://localhost:6379')");
+        tableEnvironment.executeSql("create table redis_sink(ttl bigint metadata from 'the_ttl',jim_key string,jim_value string) with('connector'='redis','redis.url'='redis://my-redis:6379')");
         tableEnvironment.executeSql("create table data_source(key string,the_value string) with('connector'='datagen','number-of-rows'='100','fields.key.length'='5','fields.the_value.length'='10')");
         tableEnvironment.executeSql("insert into redis_sink select 2000,* from data_source");
     }
