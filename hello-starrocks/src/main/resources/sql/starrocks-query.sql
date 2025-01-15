b8d3da38ae36f8e2b993879dc950e8f3e03821c7
maziqiang4 <maziqiang@jd.com>
2025-01-15 10:06:10 +0800

    增加sr查询管理的案例

--- hello-starrocks/src/main/resources/sql/starrocks-query.sql
+++ hello-starrocks/src/main/resources/sql/starrocks-query.sql
@@ -2,6 +2,15 @@
 # 我们可以给用户指定最大连接数，当超过该数量后，新的连接将会被拒绝，报以下错误。
 # Reach user-level(qualifiedUser: root, currUserIdentity: 'root'@'%') connection limit, currentUserMaxConn=1, connectionMap. size=1, connByUser. totConn=1, user. currConn=1, node=e08012839c6d:9010
 ALTER USER 'root' SET PROPERTIES ("max_user_connections" = "5");
+# 查询用户的连接数限制
+# +--------------------+---------------+
+# |Key                 |Value          |
+# +--------------------+---------------+
+# |max_user_connections|5              |
+# |catalog             |default_catalog|
+# |database            |               |
+# +--------------------+---------------+
+SHOW PROPERTY FOR 'root';
 
 # 【管理内存使用】
 # query_mem_limit：单个查询的内存限制，单位是 Byte。建议设置为 17179869184（16GB）以上。
@@ -29,6 +38,5 @@ select sleep(30);
 show processlist;
 # 我们可以使用kill命令，将要停止的sql的connection id传入，来终止正在执行的查询。
 kill 16;
-# 当查询被手动终止后，客户端会收到以下错误。
+# 当查询被手动终止后，执行查询的客户端会收到以下错误。
 # Can not read response from server. Expected to read 4 bytes, read 0 bytes before connection was unexpectedly lost.
-
